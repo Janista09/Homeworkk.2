ecc130cee00b69315a2856d628c5630e1e402fe8
๋Janista Nonsraket <68005169+Janista09@users.noreply.github.com>
2020-07-21 22:57:19 +0700

    Update workk.cpp

--- workk.cpp
+++ workk.cpp
@@ -1,27 +1,27 @@
 #include <iostream>
 #include <string>
 #include <math.h>
-using namespace std ;
-int main ()
-
+using namespace std;
+int main()
 {
 	string Name,Surname;
-	float Salary,Sale;
+	float Sale,Salary;
 	short Commission;
 	float Total;
-	cout << "*****************************************" << endl ;
-	cout << "Enter Name    : ";
+	cout << "***Homework 2**********************" << endl;
+    cout << "Enter Name   : " ;
 	cin >> Name >> Surname;
-	cout << " Enter Salary : ";
-	cin >> Salary;
-	cout << " Enter Sale   : " ;
-	cin >> Sale;
-	cout << " Enter Commission Percent : ";
-	cin >> Commission;
-	cout << "-----------------------------------------" << endl ;
-	cout << " Your Name = " << Name << " " << Surname << endl ;
-	cout << " Total Revenue " << (Sale*Commission/100)+Salary <<" "<< "Bath" << endl ;
-	cout << "-----------------------------------------" << endl ;
-	system ("pause");
-	return (0) ;
+	cout << "Enter Salary : ";
+    cin >> Salary;
+	cout << "Enter Sale   : ";
+    cin >> Sale;
+	cout << "Enter Commission Percent : ";
+    cin >> Commission;
+	cout << "------------------------------------" << endl;
+    Total = (Sale*5/100)+Salary;
+	cout << "Your Name = " << Name << " " << Surname << endl;
+	cout << "Total Revenue  " << Total << " Bath " <<  endl;
+	cout << "------------------------------------" << endl;
+system("pause");
+return (0) ;
 }
